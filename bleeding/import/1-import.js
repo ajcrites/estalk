60fb32020cb42e122a2f50ffc00c0f62a6a508de
Andrew Crites <acrites@mobiquityinc.com>
2015-10-29 11:26:03 -0400

    More thorough on import

---
+++ bleeding/import/1-import.js
@@ -0,0 +1,3 @@
+import def, {bar} from "./1-export";
+
+console.log("foo" == def, "bar" == bar);
--- bleeding/import/2-library-import.js
+++ bleeding/import/2-library-import.js
@@ -1,5 +1,5 @@
 import defaultStreamStuff, {Transform, Writable as Writer} from "stream";
-import http, * as otherHttpStuff from "http";
+import defaultHttpStuff, * as otherHttpStuff from "http";
 
 console.log(undefined !== defaultStreamStuff, undefined !== Transform, undefined !== Writer);
-console.log(undefined !== http, undefined !== otherHttpStuff);
+console.log(undefined !== defaultHttpStuff, undefined !== otherHttpStuff);
